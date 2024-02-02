94a1d248e6de3379657f560cc914deced1f0f7e8
Asif Jalil <62372647+asif-jalil@users.noreply.github.com>
2024-02-02 23:03:46 +0600

    optimize

--- next.config.mjs
+++ next.config.mjs
@@ -8,12 +8,7 @@ const nextConfig = {
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com'
-      }
-    ]
+    remotePatterns: []
   }
 };
 
