dfee5ab52126ab08b6ab3a884702d204fcdd0dae
Asif Jalil <62372647+asif-jalil@users.noreply.github.com>
2024-03-04 00:33:32 +0600

    log added

--- src/components/analytics/analytics.tsx
+++ src/components/analytics/analytics.tsx
@@ -7,10 +7,12 @@ import { GoogleAnalytics } from 'nextjs-google-analytics';
 import { isProduction } from '@/utils/check-environment';
 
 const GAnalytics = () => {
-  console.log(process.env.NEXT_PUBLIC_VERCEL_ENV);
+  console.log(process.env.NEXT_PUBLIC_VERCEL_ENV, isProduction);
   if (!isProduction) {
     return <span />;
   }
+
+  console.log('Here');
 
   return <GoogleAnalytics gaMeasurementId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID as string} trackPageViews />;
 };
